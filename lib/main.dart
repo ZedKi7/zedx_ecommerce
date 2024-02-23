56dd7ff8cac31bdaec7bb1e1f07cc73735919187
ZedKi7 <z.babaousmail@esi-sba.dz>
2024-02-23 19:40:23 +0100

    Setup essential utilities

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,7 @@
 import 'package:flutter/material.dart';
 
-import 'utils/theme/theme.dart';
+import 'app.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.system,
-      theme: ZAppTheme.lightTheme,
-      darkTheme: ZAppTheme.darkTheme,
-    );
-  }
+  runApp(const App());
 }
